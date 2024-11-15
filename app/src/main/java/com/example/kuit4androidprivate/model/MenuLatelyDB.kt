b2773797f6eef12984cf4ad8d04fb3cac7f249fc
alswlekk <michelle03212@konkuk.ac.kr>
2024-11-15 13:05:24 +0900

    Merge remote-tracking branch 'origin/main'

---
+++ app/src/main/java/com/example/kuit4androidprivate/model/MenuLatelyDB.kt
@@ -0,0 +1,28 @@
+package com.example.kuit4androidprivate.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [MenuData::class], version = 1)
+abstract class MenuLatelyDB : RoomDatabase() {
+    abstract fun menuLatelyDao(): MenuLatelyDao
+
+    companion object {
+        @Volatile
+        private var instance: MenuLatelyDB? = null
+
+        @Synchronized
+        fun getInstance(context: Context): MenuLatelyDB {
+            if (instance == null) {
+                instance = Room.databaseBuilder(
+                    context,
+                    MenuLatelyDB::class.java,
+                    "menu_lately_db"
+                ).allowMainThreadQueries().build()
+            }
+            return instance!!
+        }
+    }
+}
